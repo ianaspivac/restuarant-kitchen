243392277c98ef2fb312ec9432d352a9abcc4e34
Iana Spivac <yspivac@gmail.com>
2021-09-26 17:08:16 +0300

    minor fix

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package kitchen
+
+import (
+	"kitchen/components"
+	"kitchen/util"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+
+	const nrCooks int = 2
+	var cooks [nrCooks]*components.Cook
+	//nrApparatus := nrCooks
+	for i := 0; i <= nrCooks; i++ {
+		cooks[i] = components.HireCook(util.RandomizeNr(3))
+	}
+
+}
