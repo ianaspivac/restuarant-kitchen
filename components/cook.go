a0823faa11cc29cc705c1caa2ac0ab9f66d44de7
Iana Spivac <yspivac@gmail.com>
2021-09-26 21:36:11 +0300

    fixed cooks behaviour

--- components/cook.go
+++ components/cook.go
@@ -22,37 +22,42 @@ func HireCook(rank int) *Cook {
 	return &Cook{
 		Rank:        rank,
 		Proficiency: rank,
-		Name: "John Johnson",
+		Name:        "John Johnson",
 		CatchPhrase: "No time for talking!",
 	}
 }
-func Cooking(nrCooks int){
+func Cooking(nrCooks int) {
 	var wg sync.WaitGroup
 	wg.Add(nrCooks)
 	var m sync.Mutex
+	indexOrder := 0
 	for i := 0; i < nrCooks; i++ {
 		go func(i int) {
 			defer wg.Done()
 			for {
-				m.Lock()
-				orderPrepTime,orderIndex := GetOrderPrepTime()
-				if(orderPrepTime != 0) {
-					time.Sleep(time.Duration(orderPrepTime) * time.Second)
-					order := UpdateOrder(Order_list[orderIndex])
-					fmt.Printf("Cook %v prepared order\n",i)
-					fmt.Printf("%+v\n", order)
-					jsonBody, err := json.Marshal(order)
-					if err != nil {
-						log.Panic(err)
-					}
-					contentType := "application/json"
-					_, err = http.Post("http://localhost:8080/distribution", contentType, bytes.NewReader(jsonBody))
-					if err != nil {
-						return
+				if len(Order_list) > 0 {
+					m.Lock()
+					if len(Order_list) > indexOrder {
+						order := Order_list[indexOrder]
+						indexOrder++
+						m.Unlock()
+						fmt.Printf("Cook %v started preparing order\n", i)
+						time.Sleep(time.Duration(order.MaxPreparationTime) * time.Second)
+						fmt.Printf("Cook %v prepared order\n", i)
+						fmt.Printf("%+v\n", order)
+						jsonBody, err := json.Marshal(order)
+						if err != nil {
+							log.Panic(err)
+						}
+						contentType := "application/json"
+						_, err = http.Post("http://localhost:8080/distribution", contentType, bytes.NewReader(jsonBody))
+						if err != nil {
+							return
+						}
+					}else {
+						m.Unlock()
 					}
 				}
-				m.Unlock()
-
 			}
 		}(i)
 	}
--- components/order.go
+++ components/order.go
@@ -13,18 +13,7 @@ type Order struct {
 
 var Order_list []*Order
 
-func UpdateOrder(order *Order) *Order {
-	return &Order{
-		TableId:            order.TableId,
-		OrderId:            order.OrderId,
-		WaiterId:           order.WaiterId,
-		Priority:           order.Priority,
-		MenuItemIds:        order.MenuItemIds,
-		MaxPreparationTime: order.MaxPreparationTime,
-		PickUpTime:         order.PickUpTime,
-		Done:               true,
-	}
-}
+
 func InitOrder(order *Order) *Order {
 	return &Order{
 		TableId:            order.TableId,
@@ -38,13 +27,4 @@ func InitOrder(order *Order) *Order {
 	}
 }
 
-func GetOrderPrepTime() (float32, int) {
-	if len(Order_list) > 0 {
-		for i := 0; i <= len(Order_list); i++ {
-			if Order_list[i].Done == false {
-				return Order_list[i].MaxPreparationTime, 0
-			}
-		}
-	}
-	return 0, 0
-}
+
