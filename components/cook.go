7dee522e18dd8d4666eea0b78a59deda53015eab
Iana Spivac <yspivac@gmail.com>
2021-10-09 15:50:20 +0300

    added separation of foods to prepare order and proficiency logic fro cooks

--- components/cook.go
+++ components/cook.go
@@ -10,9 +10,10 @@ import (
 	"time"
 )
 
-const NrCooks int = 2
+const NrCooks int = 3
 
 var Cooks []*Cook
+
 
 type Cook struct {
 	Id          int
@@ -24,11 +25,11 @@ type Cook struct {
 type Rank int
 
 func HireCooks() {
-	for idx := 0; idx < NrCooks; idx++ {
+	for idx := 1; idx <= NrCooks; idx++ {
 		Cooks = append(Cooks, &Cook{
 			Id:          idx,
-			Rank:        3,
-			Proficiency: 3,
+			Rank:        idx,
+			Proficiency: idx,
 			Name:        "John Johnson",
 			CatchPhrase: "No time for talking!",
 		})
@@ -36,38 +37,78 @@ func HireCooks() {
 }
 func CooksManagement() {
 	for idx, _ := range Cooks {
-		go Cooks[idx].Cooking()
+		for foodsAtTime := 0; foodsAtTime < Cooks[idx].Proficiency; foodsAtTime++ {
+			go Cooks[idx].Cooking()
+		}
 	}
 }
-func getOrderListItem() *Order {
-	order := Order_list[0]
-	Order_list = Order_list[1:]
 
-	return order
+func getOrderListItem(rank int) FoodOrder {
+	var food FoodOrder
+	if rank == 3 && len(FoodList3.GetFoodList()) > 0 {
+		food = FoodList3.GetFoodList()[0]
+		FoodList3.ReduceFoodList()
+	} else if rank >= 2 && len(FoodList2.GetFoodList()) > 0 {
+		food = FoodList2.GetFoodList()[0]
+		FoodList2.ReduceFoodList()
+	} else {
+		food = FoodList1.GetFoodList()[0]
+		FoodList1.ReduceFoodList()
+	}
+	FoodToPrepare--
+	return food
 }
+func addToFinishedFoods(food FoodOrder) {
+	if _, alreadyExists := ReadyFood[food.orderId]; alreadyExists {
+		ReadyFood[food.orderId]++
+	} else {
+		ReadyFood[food.orderId] = 1
+	}
+	if ReadyFood[food.orderId] == food.orderSize {
+		//feel like need some semaphore or mutex
+		for idx, _ := range Order_list {
+			if Order_list[idx].OrderId == food.orderId {
+				fmt.Printf("Order was prepared: %+v\n", Order_list[idx])
+				Order_list = Order_list[1:]
+				jsonBody, err := json.Marshal(Order_list[idx])
+				if err != nil {
+					log.Panic(err)
+				}
+				contentType := "application/json"
+				_, err = http.Post("http://localhost:8080/distribution", contentType, bytes.NewReader(jsonBody))
+				if err != nil {
+					return
+				}
+				break
+			}
+		}
+	}
+}
+
 var OrderMutex sync.Mutex
+
 func (c *Cook) Cooking() {
+	//CookingAparatus := map[string]int{
+	//	"oven":2,
+	//	"stove":1,
+	//}
 	for {
 		OrderMutex.Lock()
-		if len(Order_list) > 0 {
-			order := getOrderListItem()
+		if (c.Rank == 1 && len(FoodList1.GetFoodList()) < 1)  || (c.Rank == 2 && len(FoodList2.GetFoodList()) < 1){
+			OrderMutex.Unlock()
+			continue
+		}
+		if FoodToPrepare > 0 {
+			//if(CookingAparatus["oven"] == 0 && CookingAparatus["stove"] == 0)
+			food := getOrderListItem(c.Rank)
+
 			OrderMutex.Unlock()
 
-			fmt.Printf("Cook %d started preparing order\n", c.Id)
-			time.Sleep(time.Duration(order.MaxPreparationTime) * time.Second)
+			fmt.Printf("-Cook %d started preparing food %+v\n", c.Id, food)
+			<-time.After(time.Duration(food.preparationTime) * time.Second)
+			fmt.Printf("+Cook %d finished preparing food %+v\n", c.Id, food)
+			addToFinishedFoods(food)
 
-			fmt.Printf("Cook %d prepared order\n", c.Id)
-			fmt.Printf("%+v\n", order)
-
-			jsonBody, err := json.Marshal(order)
-			if err != nil {
-				log.Panic(err)
-			}
-			contentType := "application/json"
-			_, err = http.Post("http://dining:8080/distribution", contentType, bytes.NewReader(jsonBody))
-			if err != nil {
-				return
-			}
 		} else {
 			OrderMutex.Unlock()
 		}
--- components/foods.go
+++ components/foods.go
@@ -1,80 +1,149 @@
 package components
-type Food struct{
-	id int
-	name string
-	preparationTime float32
-	complexity int
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Food struct {
+	id               int
+	name             string
+	preparationTime  float32
+	complexity       int
 	cookingApparatus string
 }
-var Menu = []Food {
+type FoodLists struct {
+	foodListMutex sync.Mutex
+	foodList []FoodOrder
+
+}
+var FoodList1 FoodLists
+var FoodList2 FoodLists
+var FoodList3 FoodLists
+var FoodToPrepare = 0
+var ReadyFood = make(map[int]int)
+
+
+type FoodOrder struct {
+	orderId       int
+	orderSize     int
+	orderPriority int
+	Food
+}
+
+
+func (f *FoodLists) GetFoodList() []FoodOrder {
+	f.foodListMutex.Lock()
+	defer f.foodListMutex.Unlock()
+
+	return f.foodList
+}
+
+func (f *FoodLists) ReduceFoodList() {
+	f.foodListMutex.Lock()
+	defer f.foodListMutex.Unlock()
+
+	f.foodList = f.foodList[1:]
+}
+
+func (f *FoodLists) SetFoodList(order *Order,idx int) {
+	f.foodListMutex.Lock()
+	defer f.foodListMutex.Unlock()
+
+	f.foodList = append(f.foodList,FoodOrder{
+		orderId:       order.OrderId,
+		orderSize:     len(order.MenuItemIds),
+		orderPriority: order.Priority,
+		Food:          Menu[idx],
+	})
+}
+
+func SeparateFoods(order *Order) {
+	for _, val := range order.MenuItemIds {
+		switch Menu[val-1].complexity {
+		case 1:
+			FoodList1.SetFoodList(order,val-1)
+		case 2:
+			FoodList2.SetFoodList(order,val-1)
+		case 3:
+			FoodList3.SetFoodList(order,val-1)
+		default:
+			fmt.Printf("Unexpected complexity")
+		}
+		FoodToPrepare++
+	}
+
+}
+
+var Menu = []Food{
 	{
-		id: 1,
-		name: "pizza",
-		preparationTime: 20 ,
-		complexity: 2 ,
+		id:               1,
+		name:             "pizza",
+		preparationTime:  20,
+		complexity:       2,
 		cookingApparatus: "oven",
 	},
 	{
-		id: 2,
-		name: "salad",
-		preparationTime: 10 ,
-		complexity: 1 ,
+		id:               2,
+		name:             "salad",
+		preparationTime:  10,
+		complexity:       1,
 		cookingApparatus: "",
 	},
 	{
-		id: 3,
-		name: "zeama",
-		preparationTime: 7 ,
-		complexity: 1 ,
+		id:               3,
+		name:             "zeama",
+		preparationTime:  7,
+		complexity:       1,
 		cookingApparatus: "stove",
 	},
 	{
-		id: 4,
-		name: "Scallop Sashimi with Meyer Lemon Confit",
-		preparationTime: 32 ,
-		complexity: 3 ,
+		id:               4,
+		name:             "Scallop Sashimi with Meyer Lemon Confit",
+		preparationTime:  32,
+		complexity:       3,
 		cookingApparatus: "",
 	},
 	{
-		id: 5,
-		name: "Island Duck with Mulberry Mustard",
-		preparationTime: 35 ,
-		complexity: 3 ,
+		id:               5,
+		name:             "Island Duck with Mulberry Mustard",
+		preparationTime:  35,
+		complexity:       3,
 		cookingApparatus: "oven",
 	},
 	{
-		id: 6,
-		name: "Waffles",
-		preparationTime: 10 ,
-		complexity: 1 ,
+		id:               6,
+		name:             "Waffles",
+		preparationTime:  10,
+		complexity:       1,
 		cookingApparatus: "stove",
 	},
 	{
-		id: 7,
-		name: "Aubergine",
-		preparationTime: 20 ,
-		complexity: 2 ,
+		id:               7,
+		name:             "Aubergine",
+		preparationTime:  20,
+		complexity:       2,
 		cookingApparatus: "",
 	},
 	{
-		id: 8,
-		name: "Lasagna",
-		preparationTime: 30 ,
-		complexity: 2 ,
+		id:               8,
+		name:             "Lasagna",
+		preparationTime:  30,
+		complexity:       2,
 		cookingApparatus: "oven",
 	},
 	{
-		id: 9,
-		name: "Burger",
-		preparationTime: 15 ,
-		complexity: 1 ,
+		id:               9,
+		name:             "Burger",
+		preparationTime:  15,
+		complexity:       1,
 		cookingApparatus: "oven",
 	},
 	{
-		id: 10,
-		name: "Gyros",
-		preparationTime: 15 ,
-		complexity: 1 ,
+		id:               10,
+		name:             "Gyros",
+		preparationTime:  15,
+		complexity:       1,
 		cookingApparatus: "",
 	},
 }
