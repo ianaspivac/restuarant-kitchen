30f484868e088436e7b84a9dc99b2f600838ea64
Iana Spivac <yspivac@gmail.com>
2021-10-09 18:58:01 +0300

    added cooking apparatus interaction

--- components/cook.go
+++ components/cook.go
@@ -13,7 +13,9 @@ import (
 const NrCooks int = 3
 
 var Cooks []*Cook
-
+var CookingApparatus map[string]int
+var semStove = make(chan int, 2)
+var semOven = make(chan int, 1)
 
 type Cook struct {
 	Id          int
@@ -46,18 +48,70 @@ func CooksManagement() {
 func getOrderListItem(rank int) FoodOrder {
 	var food FoodOrder
 	if rank == 3 && len(FoodList3.GetFoodList()) > 0 {
-		food = FoodList3.GetFoodList()[0]
-		FoodList3.ReduceFoodList()
+		idx := findByCookingApparatus(FoodList3.GetFoodList())
+		food = FoodList3.GetFoodList()[idx]
+		FoodList3.ReduceFoodList(idx)
 	} else if rank >= 2 && len(FoodList2.GetFoodList()) > 0 {
-		food = FoodList2.GetFoodList()[0]
-		FoodList2.ReduceFoodList()
+		idx := findByCookingApparatus(FoodList2.GetFoodList())
+		food = FoodList2.GetFoodList()[idx]
+		FoodList2.ReduceFoodList(idx)
 	} else {
-		food = FoodList1.GetFoodList()[0]
-		FoodList1.ReduceFoodList()
+		idx := findByCookingApparatus(FoodList1.GetFoodList())
+		food = FoodList1.GetFoodList()[idx]
+		FoodList1.ReduceFoodList(idx)
 	}
 	FoodToPrepare--
 	return food
 }
+
+func findByCookingApparatus(foodList []FoodOrder) int {
+	if CookingApparatus["oven"] == 0 && CookingApparatus["stove"] == 0 {
+		if len(foodList) == 1 && foodList[0].Food.cookingApparatus != ""{
+			switch foodList[0].Food.cookingApparatus {
+			case "oven":
+				{
+					semOven <- 1
+					CookingApparatus["oven"] -= 1
+				}
+			case "stove":
+				{
+					semStove <- 1
+					CookingApparatus["stove"] -= 1
+				}
+			}
+			return 0
+
+		}
+		for idx, _ := range foodList {
+			if foodList[idx].Food.cookingApparatus == "" {
+				return idx
+			}
+		}
+	} else {
+		for idx, _ := range foodList {
+			if foodList[idx].Food.cookingApparatus == "" {
+				return idx
+			} else if CookingApparatus[foodList[idx].Food.cookingApparatus] != 0 {
+				switch foodList[idx].Food.cookingApparatus {
+				case "oven":
+					{
+						semOven <- 1
+						CookingApparatus["oven"] -= 1
+					}
+				case "stove":
+					{
+						semStove <- 1
+						CookingApparatus["stove"] -= 1
+					}
+				}
+				return idx
+			}
+		}
+	}
+
+	return 0
+}
+
 func addToFinishedFoods(food FoodOrder) {
 	if _, alreadyExists := ReadyFood[food.orderId]; alreadyExists {
 		ReadyFood[food.orderId]++
@@ -65,7 +119,6 @@ func addToFinishedFoods(food FoodOrder) {
 		ReadyFood[food.orderId] = 1
 	}
 	if ReadyFood[food.orderId] == food.orderSize {
-		//feel like need some semaphore or mutex
 		for idx, _ := range Order_list {
 			if Order_list[idx].OrderId == food.orderId {
 				fmt.Printf("Order was prepared: %+v\n", Order_list[idx])
@@ -88,24 +141,32 @@ func addToFinishedFoods(food FoodOrder) {
 var OrderMutex sync.Mutex
 
 func (c *Cook) Cooking() {
-	//CookingAparatus := map[string]int{
-	//	"oven":2,
-	//	"stove":1,
-	//}
+	CookingApparatus = map[string]int{
+		"oven":  1,
+		"stove": 2,
+	}
 	for {
 		OrderMutex.Lock()
-		if (c.Rank == 1 && len(FoodList1.GetFoodList()) < 1)  || (c.Rank == 2 && len(FoodList2.GetFoodList()) < 1){
+		if (c.Rank == 1 && len(FoodList1.GetFoodList()) < 1) || (c.Rank == 2 && len(FoodList2.GetFoodList()) < 1) {
 			OrderMutex.Unlock()
 			continue
 		}
 		if FoodToPrepare > 0 {
-			//if(CookingAparatus["oven"] == 0 && CookingAparatus["stove"] == 0)
+
 			food := getOrderListItem(c.Rank)
 
 			OrderMutex.Unlock()
 
 			fmt.Printf("-Cook %d started preparing food %+v\n", c.Id, food)
 			<-time.After(time.Duration(food.preparationTime) * time.Second)
+
+			CookingApparatus[food.Food.cookingApparatus] += 1
+			if food.Food.cookingApparatus == "oven" {
+				<-semOven
+			} else if food.Food.cookingApparatus == "stove" {
+				<-semStove
+			}
+
 			fmt.Printf("+Cook %d finished preparing food %+v\n", c.Id, food)
 			addToFinishedFoods(food)
 
